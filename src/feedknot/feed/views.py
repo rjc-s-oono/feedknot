3a47d734d1e596a6cc057a0aea467b0d3f587d94
s_oono <s_oono@RJC016.r-jc.local>
2013-11-15 17:55:26 +0900

    feedモデル追加

--- src/feedknot/feed/views.py
+++ src/feedknot/feed/views.py
@@ -1,20 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from feed.models import Article
+from feed.models import Feed
 from time import mktime
 from datetime import datetime
 
 import feedparser
 
 def main(request):
-    feed_list=[]
-    rssurl="http://rss.dailynews.yahoo.co.jp/fc/rss.xml"
+
+    feedInfo = Feed.objects.get(id=1,box_id=1,user_id=1)
+    rssurl= feedInfo.rss_address
+    ltd = feedInfo.last_take_date
+    feedInfo.last_take_date = datetime.today()
+
+
+    article_list=[]
+
     fdp = feedparser.parse(rssurl)
     for entry in fdp['entries']:
         title = entry['title']
         link = entry['link']
         dt = datetime.fromtimestamp(mktime(entry['published_parsed']))
-        Article.objects.create(feed_id=0,box_id=1,user_id=2,article_title=title,article_address=link,pub_date=dt)
+        Article.objects.create(feed_id=1,box_id=1,user_id=1,article_title=title,article_address=link,pub_date=dt)
         #feed={'feedId':'1234',
         #       'url':link,
         #       'articleSubject':title,
@@ -31,8 +39,10 @@ def main(request):
   #         'articleSiteName':'サイト名2'}
   #  feed_list.append(feed)
 
-    feed_list = Article.objects.filter(feed_id=0,box_id=1,user_id=2)
+    feedInfo.save();
+
+    article_list = Article.objects.filter(feed_id=1,box_id=1,user_id=1)
 
     return render_to_response('feedknot/main.html',
                                {'box_title':'ボックスタイトル',
-                                'feed_list' : feed_list})
+                                'article_list' : article_list})
