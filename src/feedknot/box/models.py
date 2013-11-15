4793d8272fefcbeb5d41730a9be22945b1e4aab6
a_kurosawa <a_kurosawa@RJC045.r-jc.local>
2013-11-15 18:36:50 +0900

    以下のモデルを追加 ・ボックス

--- src/feedknot/box/models.py
+++ src/feedknot/box/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Box(models.Model):
+    user_id = models.IntegerField(u'ユーザID', max_length = 5)
+    #user_id = models.ForeignKey(User)
+    box_name = models.CharField(u'ボックス名', max_length = 255)
+    # とりあえず5段階で真ん中のイメージ
+    box_priority = models.IntegerField(u'優先度', default = 3)
+
+    def __unicode__(self):
+        return self.box_name
