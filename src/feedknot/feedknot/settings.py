f203a83e2615100d31412085ed1aaa08acc7b894
Toshiya Sugano <t_sugano@r-jc.jp>
2013-12-13 17:56:44 +0900

    Add debug-toolbar.

--- src/feedknot/feedknot/settings.py
+++ src/feedknot/feedknot/settings.py
@@ -171,35 +171,35 @@ LOGGING = {
     }
 }
 
-#if DEBUG:
-#    MIDDLEWARE_CLASSES += (
-#        'debug_toolbar.middleware.DebugToolbarMiddleware',
-#    )
-#
-#    INTERNAL_IPS = ('127.0.0.1',)
-#
-#    INSTALLED_APPS += (
-#        'debug_toolbar',
-#    )
-#
-#    DEBUG_TOOLBAR_PANELS = (
-#        'debug_toolbar.panels.version.VersionDebugPanel',
-#        'debug_toolbar.panels.timer.TimerDebugPanel',
-#        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-#        'debug_toolbar.panels.headers.HeaderDebugPanel',
-#        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-#        'debug_toolbar.panels.template.TemplateDebugPanel',
-#        'debug_toolbar.panels.sql.SQLDebugPanel',
-#        'debug_toolbar.panels.signals.SignalDebugPanel',
-#        'debug_toolbar.panels.logger.LoggingPanel',
-#    )
-#
-#    def custom_show_toolbar(request):
-#        return True # Always show toolbar, for example purposes only.
-#
-#    DEBUG_TOOLBAR_CONFIG = {
-#        'INTERCEPT_REDIRECTS': False,
-#        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
-#        #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
-#        'HIDE_DJANGO_SQL': False,
-#    }
+if DEBUG:
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+    def custom_show_toolbar(request):
+        return True # Always show toolbar, for example purposes only.
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar,
+        #'EXTRA_SIGNALS': ['myproject.signals.MySignal'],
+        'HIDE_DJANGO_SQL': False,
+    }
