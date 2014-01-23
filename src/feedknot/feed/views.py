38af1f0c23cfcd0d5421d6934ef65ee32252b818
s_oono <s_oono@RJC016.r-jc.local>
2014-01-23 19:20:32 +0900

    ボックスIDをPOSTで送信するように修正

--- src/feedknot/feed/views.py
+++ src/feedknot/feed/views.py
@@ -1,18 +1,21 @@
+from django.core.context_processors import csrf
 from django.shortcuts import render_to_response
 from box.models import Box
 from feed.models import Article
 from feed.models import Feed
 from administration.models import LoginMaster
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
 
-def main(request,user_id,box_id):
+def main(request):
+
+    user_id = 1
+    box_id = -1
+    if request.user.is_authenticated():
+        user_id=request.user.id
 
     try:
-        user_id = int(user_id)
-    except Exception:
-        user_id = -1
-    try:
-        box_id = int(box_id)
+        box_id = int(request.POST['box_id'])
     except Exception:
         box_id = -1
 
@@ -37,9 +40,12 @@ def main(request,user_id,box_id):
     article_list = Article.objects.filter(box_id=box_id).order_by('-pub_date')
     box_list = Box.objects.filter(user_id=user_id).order_by('box_priority')
 
+    param = {'user_id' : user_id,
+         'box_name' : boxName,
+         'article_list' : article_list,
+         'box_list' : box_list}
+    param.update(csrf(request))
+
     return render_to_response('feedknot/main.html',
-                               {'user_id' : user_id,
-                                'box_name' : boxName,
-                                'article_list' : article_list,
-                                'box_list' : box_list})
+                               param)
 
