574f428baff99e9a8a6fef8bec7c77738e1806a7
Masahito Shinoda <shinoda@r-jc.jp>
2014-01-24 17:06:43 +0900

    templates のパス指定を相対パスに変更。

--- src/feedknot/feedknot/settings.py
+++ src/feedknot/feedknot/settings.py
@@ -114,8 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(BASE_DIR, '../../templates'),
-    'C:/user/home/git/feedknot/src/templates',
+    os.path.join(BASE_DIR, '../../templates'),
 )
 
 INSTALLED_APPS = (
