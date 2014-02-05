af32bedb06f9201c1b4ccc1756587e6f0ac6f61a
a_kurosawa <a_kurosawa@RJC045.r-jc.local>
2014-02-05 20:55:38 +0900

    フィード追加処理を追加

--- src/feedknot/feed/views.py
+++ src/feedknot/feed/views.py
@@ -6,6 +6,8 @@ from feed.models import Feed
 from administration.models import LoginMaster
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
+import json
+from django.http.response import HttpResponse
 
 def main(request):
 
@@ -37,7 +39,7 @@ def main(request):
         except ObjectDoesNotExist:
             boxName = "NoName"
 
-    article_list = Article.objects.filter(box_id=box_id).order_by('-pub_date')
+    article_list = Article.objects.filter(box_id=box_id).order_by('-pub_date', 'id')
     box_list = Box.objects.filter(user_id=user_id).order_by('box_priority')
 
     param = {'user_id' : user_id,
@@ -48,4 +50,46 @@ def main(request):
 
     return render_to_response('feedknot/main.html',
                                param)
+
+# 返却値(失敗)
+RESPONSE_ERR = HttpResponse(json.dumps({'result': 'err'}), mimetype='application/json')
+
+# フィード追加(ajax)
+def add_feed(request):
+    box_id = -1
+    user_id = 1
+    rssaddress = ''
+    title = ''
+    
+    if not request.is_ajax():
+        return RESPONSE_ERR
+    
+    # ユーザID取得
+    if request.user.is_authenticated():
+        user_id=request.user.id
+    else:
+        user_id=1
+    
+    try:
+        rssaddress = request.POST.is_valid()
+    except Exception:
+        return HttpResponse(json.dumps({'result': '[1]koko?'}), mimetype='application/json')
+    
+    # ボックスID取得
+    try:
+        box_id = int(request.POST['box_id'])
+        rssaddress = request.POST['url']
+        title = request.POST['title']
+    except Exception:
+        return RESPONSE_ERR
+    
+    # フィード登録
+    feed = Feed(box_id=box_id, user_id=user_id,
+                rss_address=rssaddress,feed_priority=3)
+    feed.save()
+    
+    res = json.dumps({'result': 'success', 'title': title})
+    #res.update(csrf(request))
+    
+    return HttpResponse(json.dumps(res), mimetype='application/json')
 
--- src/feedknot/feedknot/urls.py
+++ src/feedknot/feedknot/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     url(r'^commonEdit/$', 'box.views.commonEdit', name='commonEdit'),
     url(r'^searchFeed/$', 'box.views.searchFeed', name='searchFeed'),
     url(r'^main/$', 'feed.views.main', name='main'),
+    url(r'^add_feed/$', 'feed.views.add_feed', name='add_feed'),
 )
 
 if settings.DEBUG:
