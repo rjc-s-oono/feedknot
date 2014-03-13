4c366a3ba63e77c58ac6b7f0a8d1a7b509052c09
a_kurosawa <a_kurosawa@RJC7052.r-jc.local>
2014-03-13 17:23:44 +0900

    フィード・記事更新 追加／ボックス追加・削除 追加

--- src/feedknot/feed/views.py
+++ src/feedknot/feed/views.py
@@ -74,9 +74,9 @@ def add_feed(request):
 
     # ユーザID取得
     if request.user.is_authenticated():
-        user_id=request.user.id
+        user_id = request.user.id
     else:
-        user_id=1
+        user_id = 1
 
     # リクエストパラメータ取得
     try:
@@ -155,3 +155,99 @@ def del_feed(request):
                 str(box_id) + ',user_id=' + str(user_id) + ',feed_id=' + str(feed_id) + ']'}), mimetype='application/json')
 
     return HttpResponse(json.dumps({'result': 'success', 'feed_id': feed_id}), mimetype='application/json')
+
+
+# 記事既読更新(ajax)
+def upd_article(request):
+    box_id = -1
+    user_id = 1
+    feed_id = 1
+    article_id = 1
+
+    if not request.is_ajax():
+        # Ajaxではない為エラー
+        return HttpResponse(json.dumps({'result': 'request is not ajax.'}), mimetype='application/json')
+
+    # ユーザID取得
+    if request.user.is_authenticated():
+        user_id = request.user.id
+    else:
+        user_id = 1
+
+    # リクエストパラメータ取得
+    try:
+        if 'box_id' in request.POST and request.POST['box_id'].isdigit():
+            box_id = int(request.POST['box_id'])
+        else:
+            box_id = 1
+        if 'feed_id' in request.POST and request.POST['feed_id'].isdigit():
+            feed_id = int(request.POST['feed_id'])
+        else:
+            feed_id = 1
+        if 'article_id' in request.POST and request.POST['article_id'].isdigit():
+            article_id = int(request.POST['article_id'])
+        else:
+            article_id = 1
+    except Exception:
+        # リクエストパラメータの取得に失敗
+        return HttpResponse(json.dumps({
+                'result': 'getting param faild.[box_id=' +
+                box_id + ',feed_id=' + feed_id + ',article_id=' + article_id + ']'}),
+                mimetype='application/json')
+
+    # 記事更新
+    try:
+        article = Article.objects.filter(box_id=box_id, user_id=user_id, feed_id=feed_id, id=article_id)
+        article.read_flg = True
+        article.save()
+    except Exception:
+        # 記事更新失敗
+        return HttpResponse(json.dumps({'result': 'update article faild.'}), mimetype='application/json')
+
+    # そのまま記事に遷移する為、何も返さない
+
+
+# フィードのボックスID更新(ajax)
+def change_box(request):
+    box_id = -1
+    user_id = 1
+    feed_id = 1
+
+    if not request.is_ajax():
+        # Ajaxではない為エラー
+        return HttpResponse(json.dumps({'result': 'request is not ajax.'}), mimetype='application/json')
+
+    # ユーザID取得
+    if request.user.is_authenticated():
+        user_id = request.user.id
+    else:
+        user_id = 1
+
+    # リクエストパラメータ取得
+    try:
+        if 'box_id' in request.POST and request.POST['box_id'].isdigit():
+            box_id = int(request.POST['box_id'])
+        else:
+            box_id = 1
+        if 'feed_id' in request.POST and request.POST['feed_id'].isdigit():
+            feed_id = int(request.POST['feed_id'])
+        else:
+            feed_id = 1
+    except Exception:
+        # リクエストパラメータの取得に失敗
+        return HttpResponse(json.dumps({
+                'result': 'getting param faild.[box_id=' +
+                box_id + ',feed_id=' + feed_id + ']'}),
+                mimetype='application/json')
+
+    # フィード更新
+    try:
+        feed = Feed.objects.filter(user_id=user_id, feed_id=feed_id)
+        feed.box_id = box_id
+        feed.save()
+    except Exception:
+        # フィード更新失敗
+        return HttpResponse(json.dumps({'result': 'update feed(box_id) faild.'}), mimetype='application/json')
+
+    return HttpResponse(json.dumps({'result': 'success', 'feed_id': feed_id}), mimetype='application/json')
+
