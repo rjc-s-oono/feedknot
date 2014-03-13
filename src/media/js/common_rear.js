86306709802653a66a2f3dc34670a0193a3c1f30
a_kurosawa <a_kurosawa@RJC7052.r-jc.local>
2014-03-13 19:58:37 +0900

    javascriptを別出しにして、base.htmlで読み込むように修正。

---
+++ src/media/js/common_rear.js
@@ -0,0 +1,88 @@
+/* == ↓jquery.mobile.js 後のjavascript ====================================== */
+
+// フィード検索機能 ロード
+google.load("feeds", "1");
+
+// フィード検索開始 (Google findFeeds)
+function searchFeed(searchTxt) {
+    //alert("検索値：" + searchTxt);
+    google.feeds.findFeeds(searchTxt, dispFeed);
+}
+
+// 検索結果 (Google findFeeds)
+function dispFeed(result) {
+    if (!result.error){
+        // エラーが発生していない場合の処理
+        if (0 < result.entries.length) {
+            $(".feed_list_ul").empty();
+
+            for (var i = 0; i < result.entries.length; i++) {
+                var title = result.entries[i].title;
+                var link = result.entries[i].link;
+                var contentSnippet = result.entries[i].contentSnippet;
+                var url = result.entries[i].url;
+
+                var tag = "<li data-theme=\"c\" id=\"feedLI\" data-icon=\"plus\" class=\"feedLI" + i + "\">" +
+                          "<a onclick=\"addFeed('#link#', '#title#','feedLI" + i + "')\">#title#</a>" +
+                          "</li>";
+                tag = tag.replace("#link#", link);
+                tag = tag.replace("#title#", title);
+                tag = tag.replace("#title#", title);
+
+                $(".feed_list_ul")
+                    .attr("data-role","listview")
+                    .append(tag);
+
+                //alert(link);
+            }
+            $(".feed_list_ul")
+                .listview().listview('refresh');
+        }
+    }
+}
+
+var isWaiting = false;
+// タッチしたフィードをDBに追加
+function addFeed(url, title, className) {
+    if (isWaiting) {
+        alert("通信中です。しばらくお待ちください。");
+        return;
+    }
+    isWaiting = true;
+
+    $("form#feed_form #url").val(url);
+    $("form#feed_form #title").val(title);
+    $("form#feed_form #className").val(className);
+    //alert("send feed." + $("form#feed_form #title").val());
+
+    // フィードを追加
+    $.ajax({
+        type: "POST",
+        url: $("form#feed_form").attr("action"),
+        data: $("form#feed_form").serialize(),
+        dataType: "json",
+        success: function(data, status, xhr) {
+            isWaiting = false;
+            //alert("status:" + status);
+            //alert("data:" + data);
+            //alert("result:" + data.result);
+            //alert($("#csrfmiddlewaretoken").val());
+            if (data == null) {
+                alert("フィードの追加に失敗しました。ログインし直してください。");
+            } else if ("success" == data.result) {
+                // 成功
+                alert("フィード【" + data.title + "】の追加が完了しました。");
+                $("." + data.className).hide();
+                $(".feed_list_ul").listview().listview('refresh');
+            } else {
+                // 失敗
+                alert("フィード【" + data.title + "】の追加に失敗しました。暫くしてから再度お試しください。");
+            }
+        },
+        error: function() {
+            isWaiting = false;
+            alert("失敗！");
+        }
+    });
+
+}
