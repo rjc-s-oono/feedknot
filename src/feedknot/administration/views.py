fd903c93bf00e463dd6058359dc97f7462d10b6d
a_kurosawa <a_kurosawa@RJC7052.r-jc.local>
2014-04-10 19:29:39 +0900

    ・初回ログイン時にボックス登録処理追加 ・パラメータが取得できない場合はエラーにする処理追加

--- src/feedknot/administration/views.py
+++ src/feedknot/administration/views.py
@@ -6,16 +6,26 @@ from django.template import RequestContext
 from django.shortcuts import render
 
 from administration.models import LoginMaster
+from box.models import Box
 
 @login_required
 def index(request):
     try:
         userInfo = LoginMaster.objects.get(user=request.user)
     except LoginMaster.DoesNotExist:
-        userInfo = LoginMaster.objects.create(user=request.user, default_box_id=-1)
+        userInfo = create(request)
 
     userName = userInfo.user.username
 
     return render(request,
                   'feedknot/Mypage.html',
                   {'login_flg':'1','user_name':userName})
+
+# ログインマスタ登録 & デフォルトボックス登録
+@login_required
+def create(request):
+    user = LoginMaster.objects.create(user=request.user, default_box_id=-1)
+    box = Box.objects.create(box_name="デフォルトボックス",user_id=user.id)
+    user.default_box_id = box.id
+    user.save()
+    return user
