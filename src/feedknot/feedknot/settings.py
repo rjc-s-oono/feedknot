9c2e58b8797e101c0b824321049ed41cee4511a3
n_matsumoto <n_matsumoto@RJC7050.r-jc.local>
2014-11-21 14:36:39 +0900

    ローカルで確認できないのでお試しに

--- src/feedknot/feedknot/settings.py
+++ src/feedknot/feedknot/settings.py
@@ -20,6 +20,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+LOGIN_REDIRECT_URL = 'account_login'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
